6554c6925fb6234c69d4cfd9fa49980548983a22
Sh031224 <1cktmdgh2@gmail.com>
2021-04-19 15:55:13 +0900

    Fix: action

--- redux-actions/src/stores/modules/counter.ts
+++ redux-actions/src/stores/modules/counter.ts
@@ -3,8 +3,8 @@ import { handleActions, createAction } from "redux-actions";
 // 뒤에 as const 를 붙여줌으로써 나중에 액션 객체를 만들게 action.type 의 값을 추론하는 과정에서
 // action.type 이 string 으로 추론되지 않고 'counter/INCREASE' 와 같이 실제 문자열 값으로 추론 되도록 해줍니다.
 
-export const INCREMENT = "counter.actions/INCREMENT" as const;
-export const DECREMENT = "counter.actions/DECREMENT" as const;
+export const INCREMENT = "counter/INCREMENT" as const;
+export const DECREMENT = "counter/DECREMENT" as const;
 
 export const increment = createAction(INCREMENT);
 export const decrement = createAction(DECREMENT);
